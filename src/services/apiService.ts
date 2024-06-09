89bff2f95f70640ae43ae9646600209afaa55b39
April.Forones <aprilforones13@gmail.com>
2024-06-10 04:15:20 +0800

    update-api

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-// export const API_BASE_URL = import.meta.env.PROD
-//   ? "/"
-//   : "http://localhost:8081";
+export const API_BASE_URL = import.meta.env.PROD
+  ? "/"
+  : "https://pmap-tqot.onrender.com";
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+  
 // Create an Axios instance
 const apiService = axios.create({
   baseURL: API_BASE_URL,
