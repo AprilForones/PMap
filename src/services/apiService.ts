e28792c550d48bb54fb217e2ab4fe31d0bbcca45
April.Forones <aprilforones13@gmail.com>
2024-06-10 04:09:46 +0800

    update-api

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 
-export const API_BASE_URL = import.meta.env.PROD
-  ? "/"
-  : "http://localhost:8081";
+// export const API_BASE_URL = import.meta.env.PROD
+//   ? "/"
+//   : "http://localhost:8081";
+
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 // Create an Axios instance
 const apiService = axios.create({
   baseURL: API_BASE_URL,
