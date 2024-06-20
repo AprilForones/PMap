5bc7b963caee98dcd1eefaf25e8632e87c1d8fb2
April.Forones <aprilforones13@gmail.com>
2024-06-20 22:46:41 +0800

    aws-update

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 
 export const API_BASE_URL = import.meta.env.PROD
   ? "/"
-  :"https://main.d2k93d841gralt.amplifyapp.com";
+  //:"https://main.d2k93d841gralt.amplifyapp.com";
   //: "http://localhost:8081";
-
+  : '/api';
 
   
 // Create an Axios instance
